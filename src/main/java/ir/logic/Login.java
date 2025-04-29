f4ad4938437505e863592583a5d2e5b2efe28a13
Maryam <maryam.sadat.jafary79@gmail.com>
2025-04-29 13:16:28 +0330

    implementing LoginTest.java and updating Login.java. also changing the pom.xml, so it fits the project better.

--- src/main/java/ir/logic/Login.java
+++ src/main/java/ir/logic/Login.java
@@ -1,4 +1,8 @@
 package ir.logic;
 
 public class Login {
+
+    public String enter(String userName, String password) {
+        return "You have logged in successfully";
+    }
 }
--- src/test/java/ir/logic/LoginTest.java
+++ src/test/java/ir/logic/LoginTest.java
@@ -1,4 +1,14 @@
 package ir.logic;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LoginTest {
+    @Test
+    public void testLogin(){
+        Login logIn = new Login();
+        String message = logIn.enter("user1","1234");
+        assertEquals("You have logged in successfully",message);
+    }
+
 }
