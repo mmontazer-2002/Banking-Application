76f0150f655f1ee8f5f0a11da96a8c878a1883e5
Maryam <maryam.sadat.jafary79@gmail.com>
2025-04-30 16:47:44 +0330

    add: User.java, Deposite.java, DepositTest.java.

--- src/main/java/ir/models/User.java
+++ src/main/java/ir/models/User.java
@@ -1,4 +1,31 @@
 package ir.models;
 
 public class User {
+    private float balance;
+    private String name;
+    private int cardNumber;
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
 }
--- src/test/java/ir/logic/DepositTest.java
+++ src/test/java/ir/logic/DepositTest.java
@@ -1,4 +1,18 @@
 package ir.logic;
 
+import ir.models.User;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DepositTest {
+
+    @Test
+    public void ifDepositIsSuccessful() {
+
+        Deposit deposit = new Deposit();
+        float initialBalance = deposit.user.getBalance();
+        float newBalance = deposit.deposit(20000);
+        assertEquals(initialBalance + 20000, newBalance);
+    }
+
 }
